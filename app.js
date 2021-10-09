9b31d6dfb32bd205b09e9545771958c2c30c5c61
jlpdeveloper <pottsman482@gmail.com>
2021-10-08 20:11:41 -0500

    added comments to make things make sense

--- app.js
+++ app.js
@@ -9,8 +9,9 @@ const { userInfo } = require('os');
 
 let members = [];
 var listCardInfo = {};
-
+//start the prompt to ask for the username / password
 prompt.start();
+//setup schema for what to ask for
 var schema = {
     properties: {
         username: {
@@ -24,6 +25,7 @@ var schema = {
 
     }
 }
+//get input and begin trello integration code
 prompt.get(schema, function (err, result) {
     if (err) { return onErr(err); }
     console.log('Command-line input received:');
@@ -32,44 +34,55 @@ prompt.get(schema, function (err, result) {
     beginTrelloIntegration(result.username, result.boardname);
 
 });
-
+//on error function
 function onErr(err) {
     console.log(err);
-
 }
-
+//function to start getting board info
 function beginTrelloIntegration(username, boardname) {
+    //get information about board
     cacheBoardInfo(username, boardname).then(boardid => {
+        //get lists from the board
         getLists(boardid).then(lists => {
+            //with this list of information, ask the user what list they want to get information from
             promptUserForLists(lists);
         });
     }).catch(onErr);
 }
+//prompt user to find out what lists to get info from
 function promptUserForLists(lists) {
-    // console.log(lists);
+    //loop through all lists and cache names
     let listNames = [];
     lists.forEach(list => {
         listNames.push(list.name);
     });
+    //ask the user what lists they want to get cards from
     let listPrompt = new promptCB({
         name: 'lists',
         message: 'What lists do you want to gather data from?',
         choices: listNames
     });
+    //run the list prompt
     listPrompt.run()
         .then(function (answers) {
-            // console.log(answers);
+            //filtered list of all lists based on answers passed in
             var selectedLists = lists.filter(list => { return answers.indexOf(list.name) >= 0 });
+            // get cards for each list. create a promise for getting list of cards, then run
+            //Promise.all to wait until all cards are gathered from all lists
             var getCardsPromiseArray = [];
             selectedLists.forEach(list => {
                 let promise = new Promise((resolve, reject) => {
+                    //get cards from trello
                     getCards(list.id).then(cards => {
+                        //add to card Info object and resolve the main process
                         listCardInfo[list.name] = cards;
                         resolve();
                     }).catch(reject);
                 });
+                //push the promise onto the all promise array
                 getCardsPromiseArray.push(promise);
             });
+            //once all the promises are done, get all the information on the cards and compile the email
             Promise.all(getCardsPromiseArray).then(values =>{
                 //construct email here
                 console.log(listCardInfo);
@@ -77,12 +90,15 @@ function promptUserForLists(lists) {
         })
         .catch(onErr);
 }
-
+//get all information about the board that you need
 function cacheBoardInfo(username, boardname) {
     let promise = new Promise((resolve, reject) => {
+        //get the board based on the board name and username
         getBoard(username, boardname).then(boardid => {
+            //get all members of the board and cache that
             getBoardMembers(boardid).then(_members => {
                 var promiseArray = [];
+                //get all information for the members
                 _members.forEach(member => {
                     promiseArray.push(getUserByID(member.idMember));
                 });
