6818e8d15b25b0607fb6a407278d356bb438afc8
jlpdeveloper <pottsman482@gmail.com>
2021-10-11 20:16:22 -0500

    updated promises for the email creator

--- email.creator.js
+++ email.creator.js
@@ -1,29 +1,41 @@
 let formatCard = require('./email.formatcard');
 const fs = require('fs');
 module.exports = function (listCardInfo, members, boardCustomFields) {
+  //create a promise to create entire email 
   var promise = new Promise((resolve, reject) => {
+    //get all keys from the lists
     var lists = Object.keys(listCardInfo);
-    email = '';
+    let email = '';
+    var lPromises = [];
+    //loop thru all the lists
     lists.forEach(list => {
+      //create a promise for each list eleement
       var listPromise = new Promise((lResolve, lReject) => {
+        //add the name of the list to the main email string
         email += '<b>' + list + '</b>\r\n\r\n';
+        //get all promises for formatting the cards
         var cardPromises = [];
         listCardInfo[list].forEach(card => {
           cardPromises.push(formatCard(card, members, boardCustomFields));
         });
+        //once all the cards are formatted, then add to the email object
         Promise.all(cardPromises).then(cards => {
           cards.forEach(_card => {
             email += _card;
           });
+          email += '\r\n\r\n';
+          //resolve the list promise
           lResolve();
         });
       });
-      Promise.all(listPromise).then(() => {
-        var now = new Date();
-
-        fs.writeFile((now.getMonth() + 1) + now.getDate() + + now.getFullYear() +  'email.txt', email).then(() => {
-          resolve(email);
-        });
+      lPromises.push(listPromise);
+      
+    });
+    //once all the list promises are complete, write the file
+    Promise.all(listPromise).then(() => {
+      var now = new Date();
+      fs.writeFile((now.getMonth() + 1) + now.getDate() + + now.getFullYear() +  'email.txt', email).then(() => {
+        resolve(email);
       });
     });
 
